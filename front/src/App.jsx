85e1998bd76f740d45781337b47e23004ab50f69
Maxim Grigorev <grmax993799@gmail.com>
2024-09-23 21:05:35 +0300

    Create front section

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import TopicList from './components/TopicList'
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <TopicList />
+        </div>
+    )
+}
+
+export default App

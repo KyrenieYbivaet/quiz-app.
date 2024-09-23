c3e13de00018768ef9b2d9ef2e533b1671db1d48
KyrenieYbivaet <gleb200320104518@yandex.ru>
2024-09-23 21:22:41 +0400

    new beautiful homepage

--- back/app.py
+++ back/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from back.models import db  # Импортируем db, чтобы инициализировать его
+from flask import Flask, render_template
+from back.models import db
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -7,9 +7,11 @@ app.config.from_object('config.Config')
 # Импортируем маршруты после создания приложения
 from back.routes import *
 
+
 @app.route('/')
 def home():
-    return "Hello, this is the Quiz app!"
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

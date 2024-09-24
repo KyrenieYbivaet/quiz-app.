7de3e29e0011ac41985583a13faebdc3591bb994
KyrenieYbivaet <gleb200320104518@yandex.ru>
2024-09-24 04:20:19 +0400

    теперь вопросы добавляются в бд корректно + переведено на русский

--- back/routes.py
+++ back/routes.py
@@ -2,14 +2,14 @@ from flask import Blueprint, request, jsonify, current_app
 from .models import db, Question
 from .translate import translate_text
 import requests
-import random
-
+from urllib.parse import unquote
 
 # Создаем Blueprint
 quiz_bp = Blueprint('quiz', __name__)
 
 # Глобальная переменная для хранения сессионного токена
 session_token = None
+
 
 # Функция для получения сессионного токена
 def get_session_token():
@@ -21,6 +21,7 @@ def get_session_token():
         else:
             raise Exception("Не удалось получить токен")
 
+
 # Функция для получения вопросов по тематике
 def get_questions_by_category(category):
     global session_token
@@ -31,14 +32,15 @@ def get_questions_by_category(category):
         "science": 17,
         "sports": 21,
         "geography": 22,
-        "math": 19
+        "math": 19,
+        "general": 9  # Добавляем категорию "General Knowledge"
     }
 
     category_id = category_ids.get(category)
     if category_id is None:
         return {"error": "Invalid category"}
 
-    trivia_url = f"https://opentdb.com/api.php?amount=5&category={category_id}&token={session_token}&encode=url3986"
+    trivia_url = f"https://opentdb.com/api.php?amount=1&category={category_id}&difficulty=easy&type=multiple&token={session_token}&encode=url3986"
     response = requests.get(trivia_url)
 
     if response.status_code != 200:
@@ -53,33 +55,32 @@ def get_questions_by_category(category):
         else:
             return {"error": "Failed to fetch questions: " + str(questions_data['response_code'])}
 
-    # Переменная для хранения валидного вопроса
-    valid_question = None
+    # Обрабатываем только один вопрос
+    item = questions_data['results'][0]
+    # Декодируем вопрос и ответы
+    decoded_question = unquote(item['question'])
+    decoded_correct_answer = unquote(item['correct_answer'])
+    decoded_options = [unquote(answer) for answer in item['incorrect_answers'] + [item['correct_answer']]]
 
-    for item in questions_data['results']:
-        translated_question = translate_text(item['question'])
-        correct_answer = translate_text(item['correct_answer'])
-        options = [translate_text(answer) for answer in item['incorrect_answers'] + [item['correct_answer']]]
+    translated_question = translate_text(decoded_question)
+    correct_answer = translate_text(decoded_correct_answer)
+    options = [translate_text(answer) for answer in decoded_options]
+    question = Question(
+        text=translated_question,
+        correct_answer=correct_answer,
+        options=",".join(options),
+        category=category
+    )
+    db.session.add(question)
+    db.session.commit()
 
-        # Проверка на существование вопроса в БД
-        existing_question = Question.query.filter_by(text=translated_question).first()
-        if existing_question is None:
-            question = Question(
-                text=translated_question,
-                correct_answer=correct_answer,
-                options=",".join(options),
-                category=category  # добавляем категорию
-            )
-            db.session.add(question)
-            db.session.commit()
-            valid_question = question
-            break  # Выходим из цикла, когда нашли валидный вопрос
+    return {
+        "text": question.text,
+        "correct_answer": question.correct_answer,
+        "options": question.options.split(","),
+        "category": question.category
+    }
 
-    if valid_question is None:
-        # Если не нашли валидного вопроса, можно попробовать снова
-        return get_questions_by_category(category)
-
-    return valid_question
 
 @quiz_bp.route('/api/questions/<category>', methods=['GET'])
 def get_questions(category):
@@ -87,11 +88,4 @@ def get_questions(category):
 
     if isinstance(question, dict) and 'error' in question:
         return jsonify(question), 500  # Ошибка при получении вопросов
-
-    return jsonify({
-        "id": question.id,
-        "text": question.text,
-        "options": question.options.split(','),
-        "category": category
-    })
-
+    return jsonify(question)
--- back/translate.py
+++ back/translate.py
@@ -1,16 +1,10 @@
 import requests
 
-def translate_text(text):
-    url = "https://libretranslate.de/translate"
-    payload = {
-        "q": text,
-        "source": "en",
-        "target": "ru",
-        "format": "text"
-    }
-    response = requests.post(url, json=payload)
 
+def translate_text(text, source_lang='en', target_lang='ru'):
+    url = f"https://ftapi.pythonanywhere.com/translate?sl={source_lang}&dl={target_lang}&text={text}"
+    response = requests.get(url)
     if response.status_code == 200:
-        return response.json()["translatedText"]
+        return response.json().get('destination-text', text)  # Возвращаем переведенный текст или исходный
     else:
-        return text  # Вернуть оригинальный текст в случае ошибки
+        raise Exception("Ошибка при переводе: " + response.text)
